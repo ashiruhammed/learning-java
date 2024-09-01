b1da9d740fa51dfc2d995115136804412506a878
ashiruhammed <hammedashiru89@gmail.com>
2024-09-01 17:35:01 +0100

    learn the conditional statements in java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,18 +3,12 @@ import java.util.Scanner;
 public class HelloWorld {
     public static void main(String args[]) {
 
-        for (int i = 1 ; i <=24; i++) {
-            int yz = i%6;
-            for (int y = i ; y <= i; y++) {
-                System.out.print("*");
-            }
-            for(int w = i; w<=i; w++) {
-                System.out.print("#");
-            }
-            for(int z = 0; z==yz; z++) {
-                System.out.println(" ");
-            }
-            System.out.println();
-        }
+        //while and do loop
+        int i =10;
+      do {
+          System.out.print(+i);
+          i--;
+      }while(i<1);
+
     }
 }
