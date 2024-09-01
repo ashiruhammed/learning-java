dd4ac8b65e26c2b5ff915f98294be95b1d5c4250
ashiruhammed <hammedashiru89@gmail.com>
2024-09-01 13:51:09 +0100

    feat: finish java syntax

---
+++ HelloWorld.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class HelloWorld {
+    public static float main(String args[]) {
+
+        int y = 3;
+        int sumXY = (y++ ) +4;
+
+        float x = 0.4f;
+
+
+        System.out.println(+y);
+        float xd = (int) (x + y);
+        System.out.print(+xd);
+        return x;
+    }
+
+
+}
