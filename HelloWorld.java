dc2875c3467057356870135c4175624e7e51e812
ashiruhammed <hammedashiru89@gmail.com>
2024-09-01 15:31:58 +0100

    feat: learn the nested loop

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,11 @@ import java.util.Scanner;
 public class HelloWorld {
     public static void main(String args[]) {
         for (int i = 0 ; i <=5; i++) {
-            System.out.println(i + " Square of " + i);
+
+            for (int y = 0 ; y <=5; y++) {
+                System.out.print("$");
+            }
+            System.out.println("*");
         }
     }
 }
