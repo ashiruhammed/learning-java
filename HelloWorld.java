932338250864837338ebd4cf90339f973b78c420
ashiruhammed <hammedashiru89@gmail.com>
2024-09-01 17:02:10 +0100

    play with inner shapes for asteriks

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,9 +2,17 @@ import java.util.Scanner;
 
 public class HelloWorld {
     public static void main(String args[]) {
-        for (int i = 1 ; i <=5; i++) {
-            for (int y = i ; y <= 5; y++) {
-                System.out.print("$");
+
+        for (int i = 1 ; i <=24; i++) {
+            int yz = i%6;
+            for (int y = i ; y <= i; y++) {
+                System.out.print("*");
+            }
+            for(int w = i; w<=i; w++) {
+                System.out.print("#");
+            }
+            for(int z = 0; z==yz; z++) {
+                System.out.println(" ");
             }
             System.out.println();
         }
