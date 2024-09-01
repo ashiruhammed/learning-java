fb381dcc7ce6735353d9bee45dad9e9cbcd64e27
ashiruhammed <hammedashiru89@gmail.com>
2024-09-01 15:50:11 +0100

    learn: inner loop

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,12 +2,11 @@ import java.util.Scanner;
 
 public class HelloWorld {
     public static void main(String args[]) {
-        for (int i = 0 ; i <=5; i++) {
-
-            for (int y = 0 ; y <=5; y++) {
+        for (int i = 1 ; i <=5; i++) {
+            for (int y = i ; y <= 5; y++) {
                 System.out.print("$");
             }
-            System.out.println("*");
+            System.out.println();
         }
     }
 }
