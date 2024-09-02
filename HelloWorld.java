7d12a0e935b578a5435c3df34475ac803167d9dd
ashiruhammed <hammedashiru89@gmail.com>
2024-09-02 11:56:26 +0100

    solve the algorithm when user type ina number and add the digits of the number together

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,12 +3,22 @@ import java.util.Scanner;
 public class HelloWorld {
     public static void main(String args[]) {
 
-        //while and do loop
-        int i =10;
-      do {
-          System.out.print(+i);
-          i--;
-      }while(i<1);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter your input number");
+
+        int userInput = scanner.nextInt();
+        int sum = 0;
+        while(userInput>0){
+           int perPart = userInput % 10;
+           if(perPart%2==0) {
+           sum+=perPart;
+           }
+           userInput = userInput/10;
+        }
+
+        System.out.print(sum);
+
 
     }
 }
