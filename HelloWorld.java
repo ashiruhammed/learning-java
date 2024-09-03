00b9183c5cf28448813a611d745673e17f68aece
ashiruhammed <hammedashiru89@gmail.com>
2024-09-04 00:58:03 +0100

    removal of an element from an array

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,15 +3,20 @@ import java.util.Scanner;
 
 public class HelloWorld {
     public static void main(String args[]) {
-        int[][] arr =  {{1,2,3}, {2,3,5}};
-       for(int i = 0; i <2; i++) {
-           for(int j = 0; j < 3; j++) {
-               System.out.print(arr[i][j] + "") ;
-           }
-               System.out.println();
-       }
 
-       int[] arrY = new int[] {1,3,4};
-       int[] arrX = new int[3];
+        int[] arr = new int[] {1,2,4};
+
+        for(int i=2; i<=arr.length-1; i++) {
+            arr[i]=arr[i-1];
+//            System.out.print()
+        }
+
+        for(int element: arr) {
+            System.out.print(element);
+        }
+
+
+
+
     }
 }
