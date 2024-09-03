5715df0c01a86e79d60e7524bc2afd8709084303
ashiruhammed <hammedashiru89@gmail.com>
2024-09-03 09:58:32 +0100

    learn the multi dimension 2D array

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,24 +1,17 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class HelloWorld {
     public static void main(String args[]) {
-
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Please enter your input number");
-
-        int userInput = scanner.nextInt();
-        int sum = 0;
-        while(userInput>0){
-           int perPart = userInput % 10;
-           if(perPart%2==0) {
-           sum+=perPart;
+        int[][] arr =  {{1,2,3}, {2,3,5}};
+       for(int i = 0; i <2; i++) {
+           for(int j = 0; j < 3; j++) {
+               System.out.print(arr[i][j] + "") ;
            }
-           userInput = userInput/10;
-        }
+               System.out.println();
+       }
 
-        System.out.print(sum);
-
-
+       int[] arrY = new int[] {1,3,4};
+       int[] arrX = new int[3];
     }
 }
