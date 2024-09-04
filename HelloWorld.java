2ba2d171dfa7563c1652455a8088994f19e6c4de
ashiruhammed <hammedashiru89@gmail.com>
2024-09-04 10:56:18 +0100

    gettting the max and min value of an array

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,16 +4,21 @@ import java.util.Scanner;
 public class HelloWorld {
     public static void main(String args[]) {
 
-        int[] arr = new int[] {1,2,4};
+        int[] arr = new int[] {-2,2,3,4,5,6,17, 98,43,343,45232};
 
-        for(int i=2; i<=arr.length-1; i++) {
-            arr[i]=arr[i-1];
-//            System.out.print()
+        int max=arr[0], min=arr[0];
+
+        for(int i=0; i<=arr.length-1; i++) {
+            if(arr[i]>max) {
+                max = arr[i];
+            }
+          else if(arr[i]<=min) {
+              min = arr[i];
+            }
         }
 
-        for(int element: arr) {
-            System.out.print(element);
-        }
+        System.out.print("max-value = "+max+" min-value = "+min);
+
 
 
 
