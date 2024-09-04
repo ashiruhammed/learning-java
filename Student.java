747ed8bfc9b36c2f7f0f0aa93728c350858ae886
ashiruhammed <hammedashiru89@gmail.com>
2024-09-04 12:13:19 +0100

    intro to oop

---
+++ Student.java
@@ -0,0 +1,19 @@
+public class Student {
+
+    String name;
+    int age;
+    String color;
+    char sex;
+
+    public void Eating() {
+        System.out.print("Eating");
+    }
+
+    public void Drinking() {
+        System.out.print("Drinking");
+    }
+
+    public void Running() {
+        System.out.print("Running");
+    }
+}
