a7aa567d45c94a51281892023289a30759dc24ad
ashiruhammed <hammedashiru89@gmail.com>
2024-09-05 04:26:00 +0100

    learning about the non access modifiesrs

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,15 +2,17 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class HelloWorld {
+
+
+    static int name = 4;
+    String a   = "As";
     public static void main(String args[]) {
 
        Student student1 = new Student("Ashiru", 10, "blacked", 'f');
-
-
-
      student1.Drinking();
 
 
     }
 
 }
+
--- Student.java
+++ Student.java
@@ -1,21 +1,4 @@
-public class Student {
-
-    String name;
-    int age;
-    String color;
-    char sex;
-
-    public void Eating() {
-        System.out.print("Eating");
-    }
-
-    public  void Drinking() {
-        System.out.print("Drinking");
-    }
-
-    public void Running() {
-        System.out.print("Running");
-    }
+public abstract class Student {
 
     public Student(String name, int age, String color, char sex) {
         this.name = name;
