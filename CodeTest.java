975bf774ae1f938e8777755cba79afffe40e97f7
ashiruhammed <111865652+ashiruhammed@users.noreply.github.com>
2024-09-18 23:06:39 +0100

    learn the method for arraylist in looping

--- CodeTest.java
+++ CodeTest.java
@@ -5,7 +5,13 @@ public class CodeTest {
     public static void main(String[] args) {
         ArrayList<String> arr = new ArrayList<>();
 
-        arr.add(0, "ashiru");
+        arr.add("element1");
+        arr.add("element2");
+        arr.add("element3");
         System.out.print(arr);
+
+        for(int i=0; i<arr.size(); i++) {
+            System.out.print(arr.get(i));
+        }
     }
 }
