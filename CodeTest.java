cf6b93fd0973c164fce6d8b2dbcd4ccfaa041459
ashiruhammed <111865652+ashiruhammed@users.noreply.github.com>
2024-09-18 22:55:09 +0100

    learn the arraylist in java

---
+++ CodeTest.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class CodeTest {
+
+    public static void main(String[] args) {
+        ArrayList<String> arr = new ArrayList<>();
+
+        arr.add(0, "ashiru");
+        System.out.print(arr);
+    }
+}
--- Student.java
+++
@@ -1,9 +0,0 @@
-public abstract class Student {
-
-    public Student(String name, int age, String color, char sex) {
-        this.name = name;
-        this.age = age;
-        this.color = color;
-        this.sex = sex;
-    }
-}
