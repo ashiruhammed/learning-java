863d8bf13c8b088f2921b560a828ab207b544d55
ashiruhammed <111865652+ashiruhammed@users.noreply.github.com>
2024-09-19 08:40:54 +0100

    learning iterable and iterators

--- CodeTest.java
+++ CodeTest.java
@@ -1,9 +1,34 @@
 import java.util.ArrayList;
 import java.util.Iterator;
 
-public class CodeTest{
+public class CodeTest {
+
+    public class MyIterable implements  Iterable<String> {
+
+        @Override
+        public Iterator<String> iterator() {
+            return null;
+        }
+
+        public class MyIterator implements  Iterator<String> {
+
+            @Override
+            public boolean hasNext() {
+                return false;
+            }
+
+            @Override
+            public String next() {
+                return "";
+            }
+
+            @Override
+            public void remove() {
+                Iterator.super.remove();
+            }
+        }
+    }
 
     public static void main(String[] args) {
-
     }
 }
